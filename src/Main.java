2f3cbc23099e48688b7641203cdf16a51fc41f4a
grmii <antoine.thomas272@gmail.com>
2025-11-28 11:56:21 +0100

    import du Main + modifications

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
 public class Main {
+
+    private static void pause() {
+        try {
+            Thread.sleep(1500L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("La pause a été interrompue : " + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
 
+        System.out.println("Bienvenue dans LA GAULE DE LINUS");
+        pause();
+        System.out.println("Nous sommes en 50 avant Jésius-Christius.");
+        pause();
+        System.out.println("Toute la Gaule est occupée par les Romains...");
+        pause();
+        System.out.println("Toute ?");
+        pause();
+        System.out.println("Non !");
+        pause();
+        System.out.println("Un petit village d'irréductibles gaulois résiste encore et toujours à l'envahisseur.");
+        pause();
+        System.out.println("Un village nommé Beurk");
+        pause();
+        System.out.println("Votre mission si vous l'acceptez, et de reconquérir la Gaule.");
 
+        pause();
+        pause();
 
+        Menu.lancerJeu();
 
     }
 }
